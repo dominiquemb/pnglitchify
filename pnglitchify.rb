e13bd95260d3d0b51402dbc06d3a3fd9ceaa7b19
Dominique Moreno-Baltierra <conejoplata@gmail.com>
2019-01-05 00:28:54 +0000

    Adding incorrect filters feature

--- pnglitchify.rb
+++ pnglitchify.rb
@@ -29,7 +29,7 @@ require 'find'
 # false
 
 filters = {'optimized' => 0, 'sub' => 1, 'up' => 2, 'average' => 3, 'paeth' => 4}
-methods = {'replace' => 0, 'transpose' => 1, 'defect' => 2}
+methods = {'replace' => 0, 'transpose' => 1, 'defect' => 2, 'graft' => 3}
 
 count = 0
 
@@ -59,6 +59,7 @@ compress = false
 alpha = false
 filter = filters[0]
 method = methods[0]
+incorrectfilter = false
 directory = ""
 
 p 'options'
@@ -97,6 +98,13 @@ options.keys.each_with_index do |key, index|
             filter = filters[filters.keys.sample]
         else
             filter = filters[chosenfilter]
+        end
+    elsif key == 'I' || key == 'incorrect'
+        chosenfilter = options[key][0]
+        if chosenfilter == 'random'
+            incorrectfilter = rand(0..4)
+        else
+            incorrectfilter = chosenfilter
         end
     elsif key == 'm' || key == 'method'
         chosenmethod = options[key][0]
@@ -137,7 +145,7 @@ p 'infiles'
 p infiles
 
 infiles.each do |infile|
-    begin
+    #begin
         unless infile.index('.png').nil?
             original_infile = infile.split('.png')[0]
         end
@@ -153,39 +161,116 @@ infiles.each do |infile|
             p directory
         end
 
+        p 'before open'
         PNGlitch.open(infile) do |p|
             buf = 2 ** 18
-            p.change_all_filters filter unless filter == filters['optimized']
 
+            unless incorrectfilter
+                p.change_all_filters filter unless filter == filters['optimized']
+            else
+                p "incorrect filter: "
+                p incorrectfilter
+                if incorrectfilter == 1 || incorrectfilter == "1"
+                    p.each_scanline do |l|
+                        l.register_filter_encoder do |data, prev|
+                            data.size.times.reverse_each do |i|
+                                x = data.getbyte(i)
+                                v = prev ? prev.getbyte(i - 1) : 0
+                                data.setbyte(i, (x - v) & 0xff)
+                            end
+                            data
+                        end
+                    end
+                end
+                if incorrectfilter == 2 || incorrectfilter == "2"
+                    p.change_all_filters 4
+                    p.each_scanline do |l|
+                        l.register_filter_encoder do |data, prev|
+                            data.size.times.reverse_each do |i|
+                                x = data.getbyte(i)
+                                v = prev ? prev.getbyte(i - 6) : 0
+                                data.setbyte(i, (x - v) & 0xff)
+                            end
+                            data
+                        end
+                    end
+                end
+                if incorrectfilter == 3 || incorrectfilter == "3"
+                    p.change_all_filters 4
+                    sample_size = p.sample_size
+                    result = ""
+                    p.each_scanline do |l|
+                        l.register_filter_encoder do |data, prev|
+                            data.size.times.reverse_each do |i|
+                                x = data.getbyte i
+                                is_a_exist = i >= sample_size
+                                is_b_exist = !prev.nil?
+                                a = is_a_exist ? data.getbyte(i - sample_size) : 0
+                                b = is_b_exist ? prev.getbyte(i) : 0
+                                c = is_a_exist && is_b_exist ? prev.getbyte(i - sample_size) : 0
+                                z =  a + b - c
+                                za = (z - a).abs
+                                zb = (z - b).abs
+                                zc = (z - c).abs
+                                zr = za <= zb && za <= zc ? a : zb <= zc ? b : c
+                                data.setbyte i, (x - zr) & 0xfe
+                            end
+                            data
+                        end
+                    end
+                end
+                if incorrectfilter == 4 || incorrectfilter == "4"
+                    p.change_all_filters 2
+                    p.each_scanline do |l|
+                        l.register_filter_encoder do |data, prev|
+                            data.size.times.reverse_each do |i|
+                                x = data.getbyte(i)
+                                v = prev ? prev.getbyte(i) : 0
+                                data.setbyte(i, (x - v) & 0xfe)
+                            end
+                            data
+                        end
+                    end
+                end
+            end
+                
             options = [filter.to_s]
             options << 'alpha' if alpha
             options << 'interlace' if interlace
             options << 'compress' if compress
             options << method.to_s
-            process = Proc.new{|data, range|
-                case method
-                when methods['replace']
-                    range.times do
-                        data[rand(data.size)] = 'x'
+
+            unless incorrectfilter
+                process = Proc.new{|data, range|
+                    case method
+                        when methods['replace']
+                            range.times do
+                                data[rand(data.size)] = 'x'
+                            end
+                            data
+                        when methods['transpose']
+                            x = data.size / 4
+                            data[0, x] + data[x * 2, x] + data[x * 1, x] + data[x * 3..-1]
+                        when methods['defect']
+                            (range / 5).times do
+                                data[rand(data.size)] = ''
+                            end
+                            data
+                        when methods['graft']
+                            p.each_scanline do |line|
+                                line.graft rand(5)
+                            end
                     end
-                    data
-                when methods['transpose']
-                    x = data.size / 4
-                    data[0, x] + data[x * 2, x] + data[x * 1, x] + data[x * 3..-1]
-                when methods['defect']
-                    (range / 5).times do
-                        data[rand(data.size)] = ''
+                }
+
+                unless compress 
+                    p.glitch do |data|
+                        process.call data, 50
                     end
-                    data
-                end
-            }
-            unless compress 
-                p.glitch do |data|
-                    process.call data, 50
-                end
-            else 
-                p.glitch_after_compress do |data|
-                    process.call data, 10
+                else 
+                    p.glitch_after_compress do |data|
+                        process.call data, 10
+                    end
                 end
             end
 
@@ -193,28 +278,22 @@ infiles.each do |infile|
             outfile = "%s_pnglitchified.png" % [original_infile]
             p 'outfile'
             p outfile
-            p.save outfile
-            p.close
+
+            unless incorrectfilter
+                p.save outfile
+                p.close
+            else
+                p.output outfile
+                p.close
+            end
 
             # deleting temporary files
             File.delete(directory + 'tmp.png') if File.exists?(directory + 'tmp.png')
-
-            #p.each_scanline do |scanline|
-            #    scanline.change_filter filter
-            #end
-            #p.glitch_as_io do |io|
-            #    until io.eof? do
-            #        d = io.read(buf)
-            #        io.pos -= d.size
-            #        io.print(d.gsub(/\d/, 'x'))
-            #    end
-            #end
-            #p.save 'dashboard_iphone_glitched.png'
         end
-    rescue
-        p 'error?'
+    #rescue
+    #    p 'error?'
         # if there's an error opening the file, just skip it
-    end
+    #end
 end
 
 #File.unlink 'tmp.png'
